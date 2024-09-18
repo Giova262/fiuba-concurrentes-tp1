256f92dfe080b9d9c9e0c49e604e5732bd3c712e
Giova262 <jogiovah@gmail.com>
2024-09-17 23:36:05 -0300

    refact

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use csv::Reader;
 use rayon::prelude::*;
 use rayon::ThreadPoolBuilder;
-
-use serde::Deserialize;
 use std::collections::HashMap;
 use std::env;
 use std::error::Error;
@@ -11,22 +9,25 @@ use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 
+use serde::Deserialize;
+use serde_json::{json, Value};
+
 #[derive(Debug, Deserialize)]
 struct Record {
     killed_by: String,
 }
 
-fn crear_archivo_salida(output_path: &str, contenido: &str) -> std::io::Result<()> {
-    // Convertir la cadena de la ruta en un objeto Path
+fn add_element_to_json(json_obj: &mut Value, key: &str, value: Value) {
+    if let Value::Object(ref mut map) = json_obj {
+        map.insert(key.to_string(), value);
+    }
+}
+
+fn create_json_file(output_path: &str, json_data: &str) -> std::io::Result<()> {
     let path = Path::new(output_path);
-
-    // Crear el archivo en la ruta especificada
-    let mut archivo = File::create(&path)?;
-
-    // Escribir el contenido en el archivo
-    archivo.write_all(contenido.as_bytes())?;
-
-    println!("Archivo creado y contenido escrito en: {}", output_path);
+    let mut file = File::create(&path)?;
+    file.write_all(json_data.as_bytes())?;
+    println!("JSON written to: {}", output_path);
     Ok(())
 }
 
@@ -34,31 +35,28 @@ fn read_and_count_kills_parallel(
     input_path: String,
     output_file_name: String,
 ) -> Result<(), Box<dyn Error>> {
-    // Abrimos el archivo CSV
+    // ARMO EL ARCHIVO
+    let mut json_obj = json!({
+        "padron": "93075",
+    });
+
+    let mut top_killers = json!({});
+    let mut top_weapons = json!({});
+    
     let mut rdr = Reader::from_path(input_path)?;
-    //let mut rdr = Reader::from_path("dataset/deaths/kill_match_stats_final_0.csv")?;
 
-    // Leemos todas las filas del CSV en memoria
-    let records: Vec<Record> = rdr
-        .deserialize()
-        .filter_map(Result::ok) // Ignoramos errores en las filas y tomamos las exitosas
-        .collect();
+    let records: Vec<Record> = rdr.deserialize().filter_map(Result::ok).collect();
 
-    // Particionamos los datos y procesamos en paralelo
     let kill_counts: HashMap<String, u32> = records
         .par_iter() // Iteramos en paralelo usando Rayon
         .map(|record| (record.killed_by.clone(), 1)) // Mapeamos cada arma a (arma, 1)
-        .fold(
-            HashMap::new, // Usamos un HashMap como acumulador local para cada thread
-            |mut acc, (weapon, count)| {
-                *acc.entry(weapon).or_insert(0) += count; // Contamos en el acumulador local
-                acc
-            },
-        )
+        .fold(HashMap::new, |mut acc, (weapon, count)| {
+            *acc.entry(weapon).or_insert(0) += count;
+            acc
+        })
         .reduce(
-            || HashMap::new(), // Inicializamos el acumulador global
+            || HashMap::new(),
             |mut acc, map| {
-                // Combinamos los acumuladores de cada thread
                 for (weapon, count) in map {
                     *acc.entry(weapon).or_insert(0) += count;
                 }
@@ -66,28 +64,33 @@ fn read_and_count_kills_parallel(
             },
         );
 
-    // Convertimos el HashMap en un vector de tuplas para ordenar
     let mut kills_vec: Vec<(String, u32)> = kill_counts.into_iter().collect();
-
-    // Ordenamos primero por la cantidad de muertes en orden descendente y luego alfabéticamente en caso de empate
     kills_vec.par_sort_by(|a, b| {
-        b.1.cmp(&a.1) // Ordenar por la cantidad de muertes (descendente)
-            .then(a.0.cmp(&b.0)) // En caso de empate, ordenar por nombre (alfabéticamente)
+        b.1.cmp(&a.1).then(a.0.cmp(&b.0)) // En caso de empate, ordenar por nombre (alfabéticamente)
     });
 
-    // Tomamos los primeros 10 resultados
+    // Me quedo con los primeros 10 resultados
     let top_10 = &kills_vec[..10.min(kills_vec.len())];
 
-    // Mostramos el top 10
     for (weapon, count) in top_10 {
-        println!("{}: {}", weapon, count);
+        add_element_to_json(
+            &mut top_weapons,
+            weapon,
+            json!({
+                "deaths_percentage": count,
+                "average_distance": count,
+                "cantidad_muertes": count,
+            }),
+        );
     }
 
-    let contenido = "Este es el contenido del archivo de salida.";
+    add_element_to_json(&mut json_obj, "top_killers", top_killers);
+    add_element_to_json(&mut json_obj, "top_weapons", top_weapons);
 
-    // Llamar a la función para crear el archivo de salida
-    if let Err(e) = crear_archivo_salida(output_file_name.as_str(), contenido) {
-        eprintln!("Error al crear el archivo: {}", e);
+    let json_data = serde_json::to_string_pretty(&json_obj).expect("Error serializing to JSON");
+
+    if let Err(e) = create_json_file(output_file_name.as_str(), &json_data) {
+        eprintln!("Error creating JSON file: {}", e);
     }
 
     Ok(())
